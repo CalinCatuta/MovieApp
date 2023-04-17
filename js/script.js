93c5b29262af7c6ad7324d680030e2bb9d1a35e7
Calin Catuta <calinut123@yahoo.co.uk>
2023-04-17 20:20:47 +0300

    Display Details and Shows

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// I need to take page link in a Object key.
+// Cream un obiect in care punem o proprietate care primeste valoarea linkului din pagina
 const global = {
     currentPage: window.location.pathname,
     api: {
@@ -6,130 +6,327 @@ const global = {
       apiUrl: 'https://api.themoviedb.org/3/'
     }
   }
-// Fetch data from API
-async function fetchDataApi(endpoint){
-   const API_KEY = global.api.apiKey
-   const API_URL = global.api.apiUrl
-
+  
+  // Fetch data from TMDB API
+  // cand va fi chemata functia fetchApiData(endpoint) vom pune fetchApiData(valoarea de aici va primi continuarea linkului in functie de ce avem nevoie din API )
+  async function fetchApiData(endpoint) {
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl;
+  
     showSpinner()
-
-   const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
-
-   const data = await response.json()
-
+    // fetch(adaugam linkul API/si/variabila din functie si cealanta variabila cu api key) nu sa pus / pentru ca sa lasat in link /
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
+  
+    const data = await response.json()
+  
     hideSpinner()
-
-   return data;
-}
-
-function showSpinner(){
-    document.querySelector('.spinner').classList.add('show')
-}
-function hideSpinner(){
-    document.querySelector('.spinner').classList.remove('show')
-}
-//O functie pentru a afisa filmele populare
-async function displayPopularMovies(){
-
-    const {results} = await fetchDataApi('movie/popular')
-
-    results.forEach(movie =>{
-        const card = document.createElement('div')
-        card.classList.add('card')
-        
-        card.innerHTML = `
-        <a href="movie-details.html?id=${movie.id}">
-                ${
-                  movie.poster_path
-                    ? `<img
-                  src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                  class="card-img-top"
+  
+    return data;
+  }
+  
+  // Display popular movie
+  async function displayPopularMovies() {
+    // aici facem Deconstruction pentru a trage din arrayul API-ul doar results. pentru ca exista in array o prioritate cu numele results.
+    const { results } = await fetchApiData('movie/popular')
+  
+    // pentru fiecare obiect facem un div cu clasa card si punem in el datele din API.
+    results.forEach(movie => {
+      const card = document.createElement('div')
+      card.classList.add('card')
+  
+      card.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              ${movie.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
                   alt="${movie.title}"
-                />`
-                    : `<img
-                src="../images/no-image.jpg"
-                class="card-img-top"
-                alt="${movie.title}"
-              />`
-                }
-              </a>
-              <div class="card-body">
-                <h5 class="card-title">${movie.title}</h5>
-                <p class="card-text">
-                  <small class="text-muted">Release: ${movie.release_date}</small>
-                </p>
-              </div>
-        `
-        document.querySelector('#popular-movies').append(card)
+                  class="card-img-top">`
+          : `<img src="../images/no-image.jpg" 
+                  alt="${movie.title}"
+                  class="card-img-top">`
+        }
+            </a>
+            <div class="card-body">
+              <h5 class="card-title">${movie.title}</h5>
+              <p class="card-text">
+                <small class="text-muted">Resale: ${movie.release_date}</small>
+              </p>
+            </div>
+          `
+  
+      document.querySelector('#popular-movies').appendChild(card)
     })
-
-}
-//O functie pentru a afisa Show-uri populare
-async function displayPopularShows(){
-    const {results} = await fetchDataApi('tv/popular')
-    results.forEach(show =>{
-        const div = document.createElement('div')
-        div.classList.add('card')
-        
-        div.innerHTML = `
-        <a href="movie-details.html?id=${show.id}">
-                ${
-                  show.poster_path
-                    ? `<img
-                  src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-                  class="card-img-top"
+  }
+  
+  // Display popular shows
+  async function displayPopularShows() {
+    const { results } = await fetchApiData('tv/popular')
+  
+    results.forEach(show => {
+      const card = document.createElement('div')
+      card.classList.add('card')
+  
+      card.innerHTML = `
+            <a href="tv-details.html?id=${show.id}">
+              ${show.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
                   alt="${show.name}"
-                />`
-                    : `<img
-                src="../images/no-image.jpg"
-                class="card-img-top"
-                alt="${show.name}"
-              />`
-                }
-              </a>
-              <div class="card-body">
-                <h5 class="card-title">${show.name}</h5>
-                <p class="card-text">
-                  <small class="text-muted">Release: ${show.first_air_date}</small>
-                </p>
-              </div>
-        `
-        document.querySelector('#popular-shows').append(div)
+                  class="card-img-top">`
+          : `<img src="../images/no-image.jpg" 
+                  alt="${show.name}"
+                  class="card-img-top">`
+        }
+            </a>
+            <div class="card-body">
+              <h5 class="card-title">${show.name}</h5>
+              <p class="card-text">
+                <small class="text-muted">Air Date: ${show.first_air_date}</small>
+              </p>
+            </div>
+          `
+  
+      document.querySelector('#popular-shows').appendChild(card)
     })
-}
-
-
-// O functie care sa adauge clasa activ nav li in functie de pagina pe care suntem
-function addActiveClass(){
+  }
+  
+  // Display movie Details
+  
+  async function displayMovieDetails() {
+    // aici cream o variabila care va extrage doar numarul id-lui.
+    const movieId = window.location.search.split('=')[1]
+  
+    // Aici tragem cu fatch din baza de date movie/id(filmului) facem asta pentru ca fiecare film are un id.
+    const movie = await fetchApiData(`movie/${movieId}`)
+  
+    // overlay for background image
+  
+    displayBackgroundImage('movie', movie.backdrop_path)
+  
+    const div = document.createElement('div')
+  
+    div.innerHTML = `
+      <div class="details-top">
+      <div>
+        ${movie.poster_path
+        ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+          alt="${movie.title}"
+          class="card-img-top">`
+        : `<img src="../images/no-image.jpg" 
+          alt="${movie.title}"
+          class="card-img-top">`
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Relase Date: ${movie.release_date}</p>
+        <p>${movie.overview}</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget: $${addCommasToNum(movie.budget)}</span></li>
+        <li><span class="text-secondary">Revenue: $${addCommasToNum(movie.revenue)}</span></li>
+        <li><span class="text-secondary">Runtime: ${movie.runtime} minutes</span></li>
+        <li><span class="text-secondary">Status: ${movie.status}</span></li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+      ${movie.production_companies
+        .map(company => `<span>${company.name}</span>`)
+        .join(', ')}
+      </div>
+    </div>
+      `
+    document.querySelector('#movie-details').appendChild(div)
+  }
+  
+  // Display Show Details
+  
+  async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1]
+    // Aici tragem cu fatch din baza de date movie/id(filmului) facem asta pentru ca fiecare film are un id.
+    const show = await fetchApiData(`tv/${showId}`)
+  
+    // overlay for background image
+  
+    displayBackgroundImage('tv', show.backdrop_path)
+  
+    const div = document.createElement('div')
+  
+    div.innerHTML = `
+      <div class="details-top">
+      <div>
+        ${show.poster_path
+        ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
+          alt="${show.name}"
+          class="card-img-top">`
+        : `<img src="../images/no-image.jpg" 
+          alt="${show.name}"
+          class="card-img-top">`
+      }
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${show.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+        <p>${show.overview}</h5>
+        <ul class="list-group">
+          ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number of Episodes: ${show.number_of_episodes}</span></li>
+        <li><span class="text-secondary">Last Episode to Air 
+        ${show.last_episode_to_air.name}</span></li>
+        <li><span class="text-secondary">Status: ${show.status}</span></li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+      ${show.production_companies
+        .map(company => `<span>${company.name}</span>`)
+        .join(', ')}
+      </div>
+    </div>
+      `
+    document.querySelector('#show-details').appendChild(div)
+  }
+  
+  // Display backdrop Details Pages
+  function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div')
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+  
+    if (type === 'movie') {
+      document.querySelector('#movie-details').appendChild(overlayDiv)
+    } else {
+      document.querySelector('#show-details').appendChild(overlayDiv)
+    }
+  }
+  
+  // Display slider movies
+  async function DisplaySlider() {
+    const { results } = await fetchApiData('movie/now_playing')
+  
+    results.forEach((movie) => {
+      const div = document.createElement('div')
+      div.classList.add('swiper-slide')
+  
+      div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+          </a>
+          <h4>
+            <i class="fas fa-star text-secondary"></i>${movie.vote_average}
+          </h4>
+      `
+      document.querySelector('.swiper-wrapper').appendChild(div)
+      initSwiper()
+    })
+  
+  }
+  // Swiper setup.
+  function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      freeMode: true,
+      loop: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 2
+        },
+        700: {
+          slidesPerView: 3
+        },
+        1200: {
+          slidesPerView: 4
+        },
+      }
+    })
+  }
+  
+  // Spinner
+  
+  function showSpinner() {
+    document.querySelector('.spinner').classList.add('show')
+  }
+  
+  function hideSpinner() {
+    document.querySelector('.spinner').classList.remove('show')
+  }
+  
+  // Active nav link.
+  function activeLi() {
     const links = document.querySelectorAll('.nav-link')
     links.forEach(link => {
-        if(link.getAttribute('href') === global.currentPage){
-            link.classList.add('active')
-        }
+      // aici in if(punem conditia ca href din <a></a> are aceasi valoare cu linkul de pe pagina actuala)
+      if (link.getAttribute('href') === pageLocation.currentPage) {
+        link.classList.add('active')
+      }
     })
-}
-
-// Create a function that load when page load.
-function init(){
-    switch(global.currentPage){
-        case '/':
-        case '/index.html':    
-            displayPopularMovies();
-            break;
-        case '/shows.html':
-            displayPopularShows();
-            break;
-        case '/movie-details.html':
-            console.log('Movie Details');
-            break;  
-        case '/tv-details.html':
-            console.log('Tv Details');
-            break;
-        case '/search.html':
-            console.log('Tv Details');
-            break;               
+  }
+  
+  // o functie care sa puna , dupa fiecare 3 numere.
+  function addCommasToNum(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+  
+  // init app
+  function init() {
+    // In aceasta functie folosim switch se putea folosi si if. 
+    // in switch(oferim valoarea linkului de pe pagina pe care ne aflam) iar cu case punem variantele posibile si in functie de caz chemam functiile.
+    switch (global.currentPage) {
+      case '/':
+      case '/index.html':
+        DisplaySlider()
+        displayPopularMovies();
+        break;
+      case '/shows.html':
+        displayPopularShows();
+        break;
+      case '/movie-details.html':
+        displayMovieDetails()
+        break;
+      case '/tv-details.html':
+        displayShowDetails()
+        break;
+      case '/search.html':
+        console.log('search');
+        break;
     }
-    addActiveClass();
-}
-
-document.addEventListener('DOMContentLoaded', init)
+  
+    activeLi()
+  }
+  
+  
+  // run function on page load.
+  document.addEventListener('DOMContentLoaded', init)
