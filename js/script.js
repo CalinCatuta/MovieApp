8633fef56125b152377ee23658c0165b755c53e2
Calin Catuta <calinut123@yahoo.co.uk>
2023-04-17 16:30:32 +0300

    Add jg code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 // I need to take page link in a Object key.
-// const global = {
-//   currentPage: 
-// }
+const global = {
+  currentPage: window.location.pathname
+}
+
+
+
+// Create a function that load when page load.
+function init(){
+    switch(global.currentPage){
+        case '/':
+        case 'index.html':    
+            console.log('Home');
+            break;
+        case 'shows.html':
+            console.log('Shows');
+            break;
+        case 'movie-details.html':
+            console.log('Movie Details');
+            break;  
+        case 'tv-details.html':
+            console.log('Tv Details');
+            break;
+        case 'tv-details.html':
+            console.log('Tv Details');
+            break;               
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init)
